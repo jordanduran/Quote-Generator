1688c3a6cc98dbb9f46edf7e448568d913987e05
Jordan Duran <jordanduran@static-192-165-1-12.cust.crystone.se>
2020-08-06 14:54:01 -0400

    Manipulating DOM for quote change and tweet quote

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
 // Get quote from API
 async function getQuote() {
   const proxyUrl = 'https://boiling-journey-68368.herokuapp.com/';
@@ -7,12 +13,36 @@ async function getQuote() {
   try {
     const response = await fetch(proxyUrl + apiUrl);
     const data = await response.json();
-    console.log(data);
+
+    // If quote doesn't have an author return Unknown
+    if (data.quoteAuthor === '') {
+      authorText.innerText = 'Unknown';
+    } else {
+      authorText.innerText = data.quoteAuthor;
+    }
+    // Reduce font size for longer quotes
+    if (data.quoteText.length > 120) {
+      quoteText.classList.add('long-quote');
+    } else {
+      quoteText.classList.remove('long-quote');
+    }
+    quoteText.innerText = data.quoteText;
   } catch (error) {
-    // getQuote();
-    console.log('Whoops! No quote', error);
+    getQuote();
   }
 }
+
+// Tweet quote
+function tweetQuote() {
+  const quote = quoteText.innerText;
+  const author = authorText.innerText;
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+  window.open(twitterUrl, '_blank');
+}
+
+// Event Listeners
+newQuoteBtn.addEventListener('click', getQuote);
+twitterBtn.addEventListener('click', tweetQuote);
 
 // On Load
 getQuote();
